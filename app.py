97bde2e78da28e9b3c9a139803e4943f55c23205
anderefelip <andregouveaf@gmail.com>
2023-10-02 22:50:23 -0300

    update html

--- app.py
+++ app.py
@@ -7,5 +7,19 @@ app.config["SECRET_KEY"] = "Secret"
 def index():
     return render_template("index.html")
 
+@app.route("/difal")
+def difal():
+
+    return render_template("difal.html")
+
+@app.route("/icms")
+def icms():
+    return render_template("icms.html")
+
+
+@app.route("/cte")
+def cte():
+    return render_template("cte.html")
+
 if __name__ == '__main__':
    app.run(debug = True)
